dbee81b2afbfd30632e87a2e2d242cc3ab11512e
Robfer Carvalho <robfer_carvalho@hotmail.com>
2022-02-04 23:05:32 -0300

    ok

---
+++ txt.js
@@ -0,0 +1,13 @@
+
+
+const periodo = ['Manhã', 'Tarde', 'Noite', 'Madrugada'];
+const sttus1 = ['Descansado', 'Revigorado' ,'Sentindo-se bem','Cansado'];
+const sttus2 =[ 'Debilitado', 'Indisposto', 'Disposto', 'Alimentado'];
+const sttus3 =['Fome', 'Frio', 'Calor', 'Sede'];
+const humor = ['Bom', 'Mau', 'Normal', 'Estressado', 'Excelente'];
+
+console.log(humor);
+console.log(sttus1);
+console.log (periodo);
+console.log(sttus2);
+console.log(sttus3);
